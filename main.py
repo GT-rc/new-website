096e59ff38b7dfd86b38f9935f664356e2778ed4
Glynnis Thomas <glynnisthomas@gmail.com>
2018-01-03 17:35:21 -0600

    Completed website setup, and tested that links worked. Definetly needs content.  :)

---
+++ main.py
@@ -0,0 +1,41 @@
+""" First attempt at making a website from scratch with Flask and Bootstrap. Wish me luck! """
+
+import os
+from flask import Flask, request, redirect, render_template, flash
+# import db stuff here
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Hookup db here - if you decide you need one //// ooh! put the contact form responses in there?
+
+@app.route("/", methods=['GET'])
+def home():
+    """ This brings up the home page. """
+    return render_template("home.html")
+
+@app.route("/about", methods=['GET'])
+def about():
+    """ This brings up the about page. """
+    return render_template("about.html")
+
+@app.route("/portfolio", methods=['GET'])
+def portfolio():
+    """ This brings up the portfolio page. """
+    return render_template("portfolio.html")
+
+@app.route("/contact", methods=['POST', 'GET'])
+def contact():
+    """ This brings up the contact page. """
+    if request.method == "POST":
+        # write logic to email info to myself
+        flash("This isn't set up yet!", 'error')
+        return redirect("/")
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run()
