1fec223eacdd302b7fbe67ffa50ea301316288a6
Glynnis Thomas <glynnisthomas@gmail.com>
2018-03-07 00:49:35 -0600

    Merged working-copy-2 branch

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ from app import app, db
 from models import ContactForm
 import jinja2
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+template_dir = os.path.join(os.path.dirname(__file__), '../templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 @app.route("/", methods=['GET'])
